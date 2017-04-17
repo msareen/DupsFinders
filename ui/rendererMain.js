1063bc703e4f3f6e0f5d35ad49ef17d7df030980
Manasvi Sareen <manasvi.sareen@gmail.com>
2017-04-17 23:39:45 +0530

    ui-grid working, custom fix in ui-grid.css

--- ui/rendererMain.js
+++ ui/rendererMain.js
@@ -1,9 +1,50 @@
-var app = angular.module('dupsFinderUI',['angular-ui-grid']);
+var app = angular.module('dupsFinderUI', ['ui.grid']);
 
-app.controller('mainController',function($scope) {
+app.controller('mainController', function ($scope) {
 
-    $scope.browseLocation = function() {
+    $scope.browseLocation = function () {
 
     }
-})
 
+    $scope.gridOptions = {
+        columnDefs: [
+            {
+                name:"firstName",
+                field:"firstName",
+            },
+            {
+                name:"lastName",
+                field:"lastName",
+            },
+            {
+                name:"company",
+                field:"company",
+            },
+            {
+                name:"employed",
+                field:"employed",
+            },
+        ]
+
+    }
+
+    $scope.gridOptions.data = [
+        {
+            "firstName": "Cox",
+            "lastName": "Carney",
+            "company": "Enormo",
+            "employed": true
+    },
+        {
+            "firstName": "Lorraine",
+            "lastName": "Wise",
+            "company": "Comveyer",
+            "employed": false
+    },
+        {
+            "firstName": "Nancy",
+            "lastName": "Waters",
+            "company": "Fuelton",
+            "employed": false
+    }];
+});
