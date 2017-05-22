b26d596dda570e3df7b93c5880b1deb3837263f9
Manasvi Sareen <manasvi.sareen@gmail.com>
2017-05-22 21:07:57 +0530

    added grid formatter.

---
+++ ui/gridFormatter.js
@@ -0,0 +1,13 @@
+(function () {
+    app.service('gridFormatterService', function (uiGridGroupingConstants) {
+        var service = this;
+
+        var columns = [
+            { name: 'id' },
+            { name: 'name' },
+            { name: 'age' },
+            { name: 'address.city' }
+        ];
+    });
+})();
+
--- ui/rendererMain.js
+++ ui/rendererMain.js
@@ -1,45 +1,47 @@
-'use strict';
+var app = (function () {
+    'use strict';
 
-var app = angular.module('dupsFinderUI', ['ui.grid']);
-const { remote, ipcRenderer } = require('electron');
+    var app = angular.module('dupsFinderUI', ['ui.grid', 'ui.grid.grouping']);
+    const { remote, ipcRenderer } = require('electron');
 
-app.controller('mainController', function ($scope) {
-    $scope.pathValue = '';
-    $scope.progressObj = {};
-    $scope.browsePath = function () {
-        $scope.pathValue = remote.dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']});
-    }
+    app.controller('mainController', function ($scope, uiGridGroupingConstants, gridFormatterService) {
+        $scope.pathValue = '';
+        $scope.progressObj = {};
+        $scope.browsePath = function () {
+            $scope.pathValue = remote.dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
+        }
 
-   $scope.lookForDuplicates = function() {
-       ipcRenderer.send('duplicate-finder', $scope.pathValue);
-       ipcRenderer.on('finder-progress', ( event, args ) => {
-           if( args.type === 'progress' ) {
-                markProgress( args.data );
-           }
-           else if ( args.type === 'result') {
-               renderResults( args.data );
-           }
-           else if( args.type === 'error' ) {
+        $scope.lookForDuplicates = function () {
+            ipcRenderer.send('duplicate-finder', $scope.pathValue);
+            ipcRenderer.on('finder-progress', (event, args) => {
+                if (args.type === 'progress') {
+                    markProgress(args.data);
+                }
+                else if (args.type === 'result') {
+                    renderResults(args.data);
+                }
+                else if (args.type === 'error') {
 
-           }
-           
-       });
-   };
+                }
 
-    function markProgress( progressObj ) {
-        $scope.progressObj = progressObj;
-        $scope.$apply();
-    }
+            });
+        };
 
-    function renderResults( results ) {
-        $scope.gridOptions.data = results.dupsHashMap;
-        $scope.$apply();
-    }
+        function markProgress(progressObj) {
+            $scope.progressObj = progressObj;
+            $scope.$apply();
+        }
 
-    $scope.gridOptions = {
-        data : {}
-    }
+        function renderResults(results) {
+            $scope.gridOptions.data = results.dupsHashMap;
+            $scope.$apply();
+        }
 
+        $scope.gridOptions = {
+            data: {}
+        }
+    });
+    return app;
+})();
 
-});
 
